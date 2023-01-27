13bcebf4908129fa644dbd45f300e3d422ad0a62
halimaHussein <hanahussein6155@gmail.com>
2023-01-27 12:48:53 +0300

    commit

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
+import random
 
 
 def home(request):
-    html_string = "<h1> hello halima</h1>"
+    
+    
+    name = 'halima'
+    number = random.randint(10,89785)
+    # from databses
+    
+    #django templates
+    html_string =f"""
+    <h1>hello {name} - {number}</h1>
+    """
+    
+    p_string =f"""
+    <p>hi {name} - {number}</p>
+    """
+    html_string = html_string+ p_string
     return HttpResponse(html_string)
